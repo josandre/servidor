1d1954a53dfd75d1377234de59dbc79cf8a3572d
Jocselyn Aguilar <ajocseline@gmail.com>
2022-10-02 22:47:52 -0600

    model ready

---
+++ server/src/models/Ingredient.ts
@@ -0,0 +1,28 @@
+import {Schema, model} from 'mongoose';
+
+export class Ingrediente{
+    name: string;
+    cost: number;
+
+    constructor(partial?: Partial<Ingrediente>) {
+        if(partial){
+            Object.assign(this, partial);
+        }
+    }
+
+}
+
+const ingredientSchema = new Schema<Ingrediente>({
+    name:{
+        type: String,
+        required: true
+    },
+    cost:{
+        type: Number,
+        required: true
+
+    }
+
+})
+
+export const ingredient = model<Ingrediente>("Ingredient", ingredientSchema);
